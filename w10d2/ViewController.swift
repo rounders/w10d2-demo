1fe60ad29b2dd4fad41a817cae1848a1e12f9e27
Francois Harbec <fharbec@gmail.com>
2017-05-15 18:05:59 -0700

    Initial Commit

---
+++ w10d2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  w10d2
+//
+//  Created by Francois Harbec on 2017-05-15.
+//  Copyright © 2017 Francois Harbec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
